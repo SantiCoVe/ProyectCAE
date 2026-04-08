f37d9016a4869668fbad647a1f1c3ba2a91ba754
SantiCoVe <cottevelasquez@gmail.com>
2026-04-08 10:45:23 -0500

    Modification of UserEntity and DocumentEntity and creation of FolderEntity and DocumentTypeEntity

--- src/main/java/com/ProyectCAE/persistenceLayer/entity/DocumentEntity.java
+++ src/main/java/com/ProyectCAE/persistenceLayer/entity/DocumentEntity.java
@@ -1,4 +1,11 @@
-package com.ProyectoCAE.persistenceLayer.entity;
+package com.ProyectCAE.persistenceLayer.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "documents")
@@ -8,20 +15,48 @@ package com.ProyectoCAE.persistenceLayer.entity;
 
 public class DocumentEntity {
 
-    @id
-    @GenerationType(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
-    private String content;
-    private String documentType;
+    private String description;
+    private String filePath;
+    private String fileType;
+    private Long documentSize;
+    private Boolean active;
 
-    @Column(name = "created_at");
+    @Column(name = "created_at")
     private LocalDateTime createdAt;
 
-    @Column(name = "updated_at");
+    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 
-    @OneToMany(mappedBy = "UserEntity", fetch = FetchType.LAZY)
-    private List<DocumentEntity> documents;
+    // Automatic Timestamps
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    // Relationship to the users table (user.id)
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity createdBy;
+
+    // Relationship to the folder table (folder.id)
+    @ManyToOne
+    @JoinColumn(name = "folder_id", nullable = false)
+    private FolderEntity folder;
+
+    // Relationship to the document_types table (id)
+    @ManyToOne
+    @JoinColumn(name = "document_type_id")
+    private DocumentTypeEntity documentType;
 }
