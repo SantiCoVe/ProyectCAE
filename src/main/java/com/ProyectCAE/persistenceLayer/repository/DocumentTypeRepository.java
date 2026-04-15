f5b78099983b2355d6fc32ceeca55f13bc033fb3
CarlosAlbeiro <carlosalbeirogaleano@hotmail.com>
2026-04-15 08:45:41 -0500

    Cambios en UserRepository.java y DocumentTypeRepository.java, verificación del springframework para la detección del IDE

--- src/main/java/com/ProyectCAE/persistenceLayer/repository/DocumentTypeRepository.java
+++ src/main/java/com/ProyectCAE/persistenceLayer/repository/DocumentTypeRepository.java
@@ -1,2 +1,13 @@
+package com.ProyectCAE.persistenceLayer.repository;
+
+import com.ProyectCAE.persistenceLayer.entity.DocumentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
 @Repository
-public interface DocumentTypeRepository extends JpaRepository<DocumentTypeEntity, Long> {}
+public interface DocumentTypeRepository extends JpaRepository<DocumentEntity, Long> {
+
+
+
+}
--- src/main/java/com/ProyectCAE/persistenceLayer/repository/UserRepository.java
+++ src/main/java/com/ProyectCAE/persistenceLayer/repository/UserRepository.java
@@ -1,4 +1,15 @@
+package com.ProyectCAE.persistenceLayer.repository;
+
+import com.ProyectCAE.persistenceLayer.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByEmail(String email);
+    Optional<UserEntity> findByPhone(String phone);
 
 }
